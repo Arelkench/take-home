d22186ebc4e6ec100d54c560bff2fcc4dd2d45b7
Arelkench <nefera2801@gmail.com>
2024-11-18 19:25:00 +0100

    IsDeletedCardsVisible -> areDeletedCardsVisible, add useCallbacks, fix refresh logic

--- src/components/Entrypoint.tsx
+++ src/components/Entrypoint.tsx
@@ -1,25 +1,31 @@
-import { useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 import { useGetListData } from "../api/getListData";
 import { Spinner } from "./Spinner";
 import { List } from "./List.tsx";
 import { useCardStore } from "../store/store.ts";
 
 export const Entrypoint = () => {
+    const [areDeletedCardsVisible, setAreDeletedCardsVisible] = useState(false);
+
     const listQuery = useGetListData();
+
     const { visibleCards, deletedCards, initializeCards } = useCardStore();
-    const [isDeletedCardsVisible, setIsDeletedCardsVisible] = useState(false);
 
-    const handleRevealClick = () => {
-        const newVisibility = !isDeletedCardsVisible;
-        setIsDeletedCardsVisible(newVisibility);
 
-        localStorage.setItem("deletedCardsVisible", JSON.stringify(newVisibility));
-    };
+    const handleRevealClick = useCallback(() => {
+        setAreDeletedCardsVisible((prevState) => !prevState);
+        localStorage.setItem("deletedCardsVisible", JSON.stringify(areDeletedCardsVisible));
+    },[areDeletedCardsVisible])
+
+    const handleRefreshClick = useCallback(async () => {
+        await listQuery.refetch();
+    },[listQuery])
 
     useEffect(() => {
-        if (listQuery.isLoading) {
+        if (listQuery.isLoading || listQuery.isFetching) {
             return;
         }
+
 
         const storedVisibleCards = localStorage.getItem("visibleCards");
         const storedDeletedCards = localStorage.getItem("deletedCards");
@@ -34,29 +40,41 @@ export const Entrypoint = () => {
         }
 
         const storedVisibility = localStorage.getItem("deletedCardsVisible");
-        if (storedVisibility) {
-            setIsDeletedCardsVisible(JSON.parse(storedVisibility));
-        }
-    }, [listQuery.data, listQuery.isLoading, initializeCards]);
 
-    if (listQuery.isLoading) return <Spinner />;
+        if (storedVisibility) {
+            setAreDeletedCardsVisible(JSON.parse(storedVisibility));
+        }
+    }, [listQuery.data, listQuery.isLoading, initializeCards, listQuery.isFetching]);
+
+
+    if (listQuery.isLoading || listQuery.isFetching) {
+        return <Spinner />;
+    }
 
     return (
         <div className="flex gap-x-16 w-full justify-center align-top">
             <div className="w-full max-w-xl">
                 <List items={visibleCards} isVisible={true} title="My Awesome List" />
             </div>
-            <button
-                onClick={handleRevealClick}
-                className="text-white text-sm font-semibold transition-colors hover:bg-gray-800 bg-black rounded px-6 py-2 h-9"
-            >
-                Reveal
-            </button>
+            <div className="flex flex-col gap-y-2">
+                <button
+                    onClick={handleRevealClick}
+                    className="text-white text-sm font-semibold transition-colors hover:bg-gray-800 bg-black rounded px-6 py-2 h-9"
+                >
+                    Reveal
+                </button>
+                <button
+                    onClick={handleRefreshClick}
+                    className="text-white text-sm font-semibold transition-colors hover:bg-gray-800 bg-red-600 rounded px-6 py-2 h-9"
+                >
+                    Refresh
+                </button>
+            </div>
             <div className="w-full max-w-xl">
                 <div className="flex flex-col gap-y-3">
                     <List
                         items={deletedCards}
-                        isVisible={isDeletedCardsVisible}
+                        isVisible={areDeletedCardsVisible}
                         title="Deleted Cards"
                     />
                 </div>
